deaaa9fe8a7effd29edeb8fc3bc15dc2acf60586
RashanWeerasinghe <malithrashan03@gmail.com>
2023-09-27 16:41:33 +0530

    Display Data

--- news-web/src/App.jsx
+++ news-web/src/App.jsx
@@ -1,35 +1,104 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import axios from "axios";
+import Card from "@mui/material/Card";
+import CardActionArea from "@mui/material/CardActionArea";
+import CardHeader from "@mui/material/CardHeader";
+import CardMedia from "@mui/material/CardMedia";
+import CardContent from "@mui/material/CardContent";
+import Typography from "@mui/material/Typography";
+import Box from "@mui/material/Box";
+import { articles } from "./data";
+import Grid from "@mui/material/Grid";
+import Button from "@mui/material/Button";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [data, setData] = useState(null);
+
+  // const baseUrl =
+  //   "https://newsapi.org/v2/top-headlines?country=us&apiKey=";
+  useEffect(() => {
+    // axios
+    //   .get(baseUrl)
+    //   .then((result) => {
+    //     const sorted = result.data.articles.sort(
+    //       (a, b) => new Date(b.publishedAt) - new Date(a.publishedAt)
+    //     );
+    //     setData(sorted);
+    //   })
+    //   .catch((err) => console.log(err));
+
+    const sorted = articles.sort(
+      (a, b) => new Date(b.publishedAt) - new Date(a.publishedAt)
+    );
+    console.log(sorted);
+    setData(sorted);
+  }, [data]);
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div style={{ width: "100%" }}>
+      <Box
+        sx={{
+          display: "flex",
+          gridTemplateColumns: "repeat(4, 1fr)",
+          gap: 1,
+          m: 1,
+          p: 1,
+          gridTemplateRows: "auto",
+        }}
+      >
+        <Grid container spacing={2}>
+          {data == null
+            ? null
+            : data.map((res) => {
+                return res.source.id == null ? null : (
+                  <Grid item xs={12} sm={6} md={4} key={res.url} spacing={5}>
+                    <Card>
+                      <CardActionArea>
+                        <a style={{ textDecoration: "none" }} href={res.url}>
+                          <CardHeader
+                            title={res.title}
+                            subheader={`by ${res.author} - ${res.source.name} `}
+                            titleTypographyProps={{ align: "left" }}
+                            subheaderTypographyProps={{ align: "left" }}
+                          />
+                        </a>
+                        <CardContent>
+                          <Typography
+                            align="left"
+                            variant="body2"
+                            color="text.secondary"
+                          >
+                            {new Date(res.publishedAt).toLocaleDateString()}
+                          </Typography>
+                        </CardContent>
+                        <Box sx={{ display: "flex", flexDirection: "row" }}>
+                          <img
+                            src={res.urlToImage}
+                            style={{ width: 150, height: 150, padding: 10 }}
+                            alt="example"
+                          />
+
+                          <CardContent>
+                            <Typography variant="body2" color="text.secondary">
+                              {res.content}
+                              <a
+                                style={{ textDecoration: "none" }}
+                                href={res.url}
+                              >
+                                <Button size="small">Read More</Button>
+                              </a>
+                            </Typography>
+                          </CardContent>
+                        </Box>
+                      </CardActionArea>
+                    </Card>
+                  </Grid>
+                );
+              })}
+        </Grid>
+      </Box>
+    </div>
+  );
 }
 
-export default App
+export default App;
